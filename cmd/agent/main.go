480bc27010deae81482634d06f8c3a1769386d8c
Igor Polishchuk <igor.policee@gmail.com>
2024-12-06 22:41:42 +0300

    feature: sending gauge metric "RandomValue"

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/igor-policee/metrics-alerts-service/cmd/agent/sender"
 	"github.com/igor-policee/metrics-alerts-service/cmd/agent/storage"
+	"math/rand/v2"
 	"strconv"
 	"strings"
 	"time"
@@ -27,11 +28,20 @@ func main() {
 			}
 		}
 
-		metricType = "counter"
-		metricName := "PollCount"
-		metricValue := strconv.Itoa(1)
+		metricType = "gauge"
+		metricName := "RandomValue"
+		randomNumber := rand.Int()
+		metricValue := strconv.Itoa(randomNumber)
 		endpointSlice := []string{serverAddress, metricOperation, metricType, metricName, metricValue}
 		endpointString := strings.Join(endpointSlice, "/")
+		fmt.Println(endpointString)
+		sender.SendPostRequest(endpointString)
+
+		metricType = "counter"
+		metricName = "PollCount"
+		metricValue = strconv.Itoa(1)
+		endpointSlice = []string{serverAddress, metricOperation, metricType, metricName, metricValue}
+		endpointString = strings.Join(endpointSlice, "/")
 		sender.SendPostRequest(endpointString)
 
 		time.Sleep(pollInterval * time.Second)
