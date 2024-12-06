e0df2f15eab49573fe6959a37801ff5b37ca797a
Igor Polishchuk <igor.policee@gmail.com>
2024-12-06 18:13:54 +0300

    feature: add storage package

---
+++ cmd/agent/storage/storage.go
@@ -0,0 +1,30 @@
+package storage
+
+import (
+	"reflect"
+	"runtime"
+	"time"
+)
+
+func CollectMetrics() map[string]map[string]uint64 {
+	metricStorage := make(map[string]map[string]uint64)
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	v := reflect.ValueOf(memStats)
+	typeOfMemStats := v.Type()
+
+	now := time.Now().Format(time.RFC3339)
+	metricStorage[now] = make(map[string]uint64)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := typeOfMemStats.Field(i).Name
+		fieldValue := v.Field(i)
+
+		if fieldValue.Kind() == reflect.Uint64 {
+			metricStorage[now][field] = fieldValue.Uint()
+		}
+	}
+
+	return metricStorage
+}
