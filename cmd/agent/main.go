6e39fe794fc1689ac78c520739e1fa904b507907
Igor Polishchuk <igor.policee@gmail.com>
2024-12-06 17:23:26 +0300

    feature: add a template for collecting metrics

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"time"
+)
+
+func main() {
+	fmt.Println("--> Hello Gophers! <--")
+
+	for t, metrics := range collectMetrics() {
+		for metric, value := range metrics {
+			fmt.Println(t, metric, value)
+		}
+	}
+
+	fmt.Println("--> Good Bye Gophers! <--")
+}
+
+func collectMetrics() map[string]map[string]uint64 {
+	metricStorage := make(map[string]map[string]uint64)
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	v := reflect.ValueOf(memStats)
+	typeOfMemStats := v.Type()
+
+	now := time.Now().Format(time.RFC3339)
+	metricStorage[now] = make(map[string]uint64)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := typeOfMemStats.Field(i).Name
+		fieldValue := v.Field(i)
+
+		if fieldValue.Kind() == reflect.Uint64 {
+			metricStorage[now][field] = fieldValue.Uint()
+		}
+	}
+
+	return metricStorage
+}
